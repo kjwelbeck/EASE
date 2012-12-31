abefd89cc6353cadd387dde30985e552e1bfa586
Kojo Welbeck <kojo.welbeck99@gmail.com>
2012-12-31 15:23:26 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EaseApp::Application.config.secret_token = '50677ed79d29e4314252c98a2b2060a13e84a8f0091945f17b8f921a293337bb40d60dbb57cef2c43f93bcb14035b67332006b9a78139cd143cf55cd55b0919e'
